4e47100a8f412c91c473b80de97839cd167b0113
Ward Vandewege <ward@jhvc.com>
2020-07-05 17:01:42 -0400

    luci-app-autoygg: expose the information from the gateway /info endpoint.

--- luci-app-autoygg/files/www/luci-static/resources/view/autoygg/status.js
+++ luci-app-autoygg/files/www/luci-static/resources/view/autoygg/status.js
@@ -51,16 +51,42 @@ function init_view() {
 	view.appendChild(info_title);
   view.appendChild(button_div);
 	view.appendChild(self_info);
+
+	var gateway_info_title = document.createElement("h2"); gateway_info_title.innerText = _("Autoygg gateway info");
+	var gateway_info = document.createElement("div"); gateway_info.setAttribute("class", "table");
+	var gateway_data = {
+		"Owner": "gatewayowner",
+		"Description": "description",
+		"Gateway information URL": "gatewayinfourl",
+		"Software version": "softwareversion",
+		"Network": "network",
+		"Location": "location",
+		"Registration required": "requireregistration",
+		"Approval required": "requireapproval",
+		"Access List Enabled": "accesslistenabled"
+	};
+
+	Object.keys(gateway_data).forEach(function(k) {
+		var tr = document.createElement("div");
+		tr.setAttribute("class", "tr");
+		var td1 = document.createElement("div"); td1.setAttribute("class", "td left");
+		td1.textContent = k;
+		var td2 = document.createElement("div"); td2.setAttribute("class", "td left");
+		td2.id = gateway_data[k];
+
+		tr.appendChild(td1); tr.appendChild(td2); gateway_info.appendChild(tr);
+	});
+
+	view.appendChild(gateway_info_title);
+	view.appendChild(gateway_info);
+
 	return view;
 }
 
 function autoygg_toggle(id) {
-  console.log(id);
-
 	var view = document.getElementById("view");
   var action, newButtonLabel;
 
-  console.log(view.querySelector('#state').innerText);
   if (view.querySelector('#state').innerText != "connected") {
     action = "--action=register";
     id.srcElement.innerText = "Connecting...";
@@ -70,13 +96,13 @@ function autoygg_toggle(id) {
   }
 	fs.exec("/usr/sbin/autoygg-client", ["--useUCI",action]).then(function(res){
     if (res && res.code === 0) {
-      reload_view(view);
+      reload_client_status(view);
     }
   });
   return;
 }
 
-function update_view(view,obj) {
+function update_client_status(view,obj) {
 	view.querySelector('#state').innerText = obj.state;
 	view.querySelector('#desiredstate').innerText = obj.desiredstate;
 	view.querySelector('#error').innerText = obj.error;
@@ -98,11 +124,24 @@ function update_view(view,obj) {
   }
 }
 
-function reload_view(view) {
+function update_gateway_info(view,obj) {
+	view.querySelector('#gatewayowner').innerText = obj.GatewayOwner;
+	view.querySelector('#description').innerText = obj.Description;
+	view.querySelector('#gatewayinfourl').innerHTML = "<a href=\"" + obj.GatewayInfoURL + "\">" + obj.GatewayInfoURL + "</a>";
+	view.querySelector('#softwareversion').innerText = obj.SoftwareVersion;
+	view.querySelector('#network').innerText = obj.Network;
+	view.querySelector('#location').innerText = obj.Location;
+	view.querySelector('#requireregistration').innerText = obj.RequireRegistration;
+	view.querySelector('#requireapproval').innerText = obj.RequireApproval;
+	view.querySelector('#accesslistenabled').innerText = obj.AccessListEnabled;
+}
+
+function reload_info(view) {
+	// We do not reload the gateway information, there is little point as it should be quite static
 	fs.exec("/usr/sbin/autoygg-client", ["--state"]).then(function(res){
 		if (res && res.code === 0) {
 			var obj = JSON.parse(res.stdout.trim());
-			update_view(view,obj);
+			update_client_status(view,obj);
 		}
 	});
 }
@@ -111,7 +150,8 @@ return view.extend({
 	load: function() {
 		return Promise.all([
 			L.resolveDefault(fs.stat("/usr/sbin/autoygg-client"), null),
-			L.resolveDefault(fs.exec("/usr/sbin/autoygg-client", ["--state"]), null)
+			L.resolveDefault(fs.exec("/usr/sbin/autoygg-client", ["--state"]), null),
+			L.resolveDefault(fs.exec("/usr/sbin/autoygg-client", ["--useUCI", "--action=info"]), null)
 		]);
 	},
 	render: function(info) {
@@ -119,10 +159,14 @@ return view.extend({
 
 		if (info[0] && info[1] && info[1].code === 0) {
 			var obj = JSON.parse(info[1].stdout.trim());
-			update_view(view,obj);
-			setInterval(reload_view, 5000, view);
+			update_client_status(view,obj);
+			setInterval(reload_info, 5000, view);
 		} else {
 			view.innerHTML = "<h2>Unable to read autoygg client state</h2>";
+		}
+		if (info[0] && info[2] && info[2].code === 0) {
+			var obj = JSON.parse(info[2].stdout.trim());
+			update_gateway_info(view,obj);
 		}
 		return view;
 	},
