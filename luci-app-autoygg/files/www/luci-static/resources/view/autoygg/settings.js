98c837388e0be8e59678e7e0d392bfc39ee87d66
JamesVorder <james@bip44.email>
2020-09-27 20:32:26 -0400

    Basic saved gateways utility.

--- luci-app-autoygg/files/www/luci-static/resources/view/autoygg/settings.js
+++ luci-app-autoygg/files/www/luci-static/resources/view/autoygg/settings.js
@@ -4,9 +4,9 @@
 
 return view.extend({
 	render: function() {
-		var m, s, o;
+		var m, s, o, saved_gateways_section;
 
-		m = new form.Map('autoygg', 'Autoygg-client');
+		m = new form.Map('autoygg', 'Internet Gateway Settings');
 
 		s = m.section(form.TypedSection, 'autoygg', _('General settings'));
 		s.anonymous = true;
@@ -18,6 +18,11 @@ return view.extend({
 		s.option(form.Value, "clientname", _("Your name (optional)"));
 		s.option(form.Value, "clientemail", _("Your e-mail address (optional)"));
 		s.option(form.Value, "clientphone", _("Your phone number (optional)"));
+
+		saved_gateways_section = m.section(form.TypedSection, 'autoygg', _('Saved Gateways'));
+		saved_gateways_section.anonymous = true;
+
+		saved_gateways_section.option(form.DynamicList, "gateway", _("Saved gateways"));
 
 		return m.render();
 	}
