be4ecc8d7a261fcbafacb144168dd2168905dd22
JamesVorder <jamesvorder@gmail.com>
2020-11-29 14:41:33 -0500

    Basic add/remove functionality.

--- luci-app-autoygg/files/www/luci-static/resources/view/autoygg/settings.js
+++ luci-app-autoygg/files/www/luci-static/resources/view/autoygg/settings.js
@@ -8,16 +8,25 @@ return view.extend({
 
 		m = new form.Map('autoygg', 'Autoygg-client');
 
-		s = m.section(form.TypedSection, 'autoygg', _('General settings'));
+		s = m.section(form.TypedSection, 'autoygg', _('Gateway Selection'));
 		s.anonymous = true;
 
-		s.option(form.Value, "gatewayhost", _("the yggdrasil ipv6 address of your autoygg gateway"));
-		s.option(form.Value, "gatewayport", _("the port of your autoygg gateway"));
-		s.option(form.Value, "yggdrasilinterface", _("Yggdrasil's network interface name"));
+		s.option(form.Value, "gatewayhost", _("Gateway Yggdrasil IPv6"));
+		s.option(form.Value, "gatewayport", _("Gateway Port (default 8080)"));
+
+		s = m.section(form.TypedSection, 'autoygg', _('Personal Settings'));
+		s.anonymous = true;
 
 		s.option(form.Value, "clientname", _("Your name (optional)"));
 		s.option(form.Value, "clientemail", _("Your e-mail address (optional)"));
 		s.option(form.Value, "clientphone", _("Your phone number (optional)"));
+		s.option(form.Value, "yggdrasilinterface", _("Yggdrasil Network Interface"));
+
+		s = m.section(form.TableSection, 'saved_gateway', _('Saved Gateways'));
+		s.option(form.Value, "host", _("Host"));
+		s.option(form.Value, "port", _("Port (default 8080)"));
+		s.anonymous = true;
+		s.addremove = true;
 
 		return m.render();
 	}
